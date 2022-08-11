345785f346a7f3fa5a16e49d22e7e7eb9ffa63f4
zzxihep <zzx@bigtree.xyz>
2022-08-12 01:03:30 +0800

    add a plot_info.py used to extract the plotter elements from SEDPlotter

---
+++ astroARIADNE/plotter_info.py
@@ -0,0 +1,28 @@
+from .plotter import SEDPlotter
+
+
+def get_spectrum_model(artist: SEDPlotter):
+    """
+    Get the spectrum model from the artist.
+    """
+    Rv = 3.1
+    if not artist.norm:
+        rad = artist.theta[4]
+        dist = artist.theta[3] * u.pc.to(u.solRad)
+        norm = (rad / dist) ** 2
+        Av = artist.theta[5]
+    else:
+        norm = artist.theta[3]
+        Av = artist.theta[4]
+    return artist.spectrum_model
+    if artist.grid == 'btcond':
+        wave, flux = artist.fetch_btcond()
+        lower_lim = 0.125 < wave
+        upper_lim = wave < 4.629296073126975
+        if artist.irx:
+            upper_lim = wave < wave[-1]
+        wave = wave[lower_lim * upper_lim]
+        flux = flux[lower_lim * upper_lim]
+        ext = self.av_law(wave * 1e4, Av, Rv)
+        flx = apply(ext, flux)
+        return wave, flx
